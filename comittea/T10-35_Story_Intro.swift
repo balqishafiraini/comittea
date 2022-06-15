525d25ad65de579d98efd3af3f72afaebb56b7bf
dennisanthonyblabla <dennisanthonyblabla@gmail.com>
2022-06-15 14:53:35 +0700

    Nav Bar dan Image View Done

---
+++ comittea/T10-35_Story_Intro.swift
@@ -0,0 +1,29 @@
+//
+//  T10-35_Story_Intro.swift
+//  comittea
+//
+//  Created by Dennis Anthony on 15/06/22.
+//
+
+import UIKit
+
+class T10_35_Story_Intro: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
