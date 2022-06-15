fed61a9f080369e471e46beb1d90186d67ccb037
dennisanthonyblabla <dennisanthonyblabla@gmail.com>
2022-06-15 17:49:21 +0700

    thumbnail sudah diubah semua tapi Constraint custom cell belum berjalan dengan baik, Garis separator juga belum bisa hilang.

--- comittea/T10-35_Story_Intro.swift
+++ comittea/T10-35_Story_Intro.swift
@@ -12,7 +12,6 @@ class T10_35_Story_Intro: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
