b4ec74fb899a3456a17ea7749d49f7b014c18022
dennisanthonyblabla <dennisanthonyblabla@gmail.com>
2022-06-17 13:29:31 +0700

    Asset untuk Intermezo sudah masuk, baru test pakai yang planning ahead. Button sudah bisa digunakan tapi belum diberikan fungsi. masih menunggu halaman lain.

---
+++ comittea/ViewController/T10-36_Story Intermezzo.swift
@@ -0,0 +1,26 @@
+//
+//  T10-36_Story Intermezzo.swift
+//  comittea
+//
+//  Created by Dennis Anthony on 17/06/22.
+//
+
+import UIKit
+
+class T10_36_Story_Intermezzo: UIViewController {
+
+    @IBOutlet weak var Intermezzo_Thumbnail: UIImageView!
+    @IBOutlet weak var Btn_NextChapter: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Intermezzo_Thumbnail.image = UIImage(named: "Intermezzo_Calender"
+        )
+    }
+    
+    @IBAction func Btn_NextChapter(_ sender: Any) {
+        
+    }
+    
+}
