38e7ac13922c86738cdd3b96b869fbad424901a8
dennisanthonyblabla <dennisanthonyblabla@gmail.com>
2022-06-17 15:48:07 +0700

    Tombol sudah bulat cornernya

--- comittea/ViewController/T10-36_Story Intermezzo.swift
+++ comittea/ViewController/T10-36_Story Intermezzo.swift
@@ -10,13 +10,18 @@ import UIKit
 class T10_36_Story_Intermezzo: UIViewController {
 
     @IBOutlet weak var Intermezzo_Thumbnail: UIImageView!
-    @IBOutlet weak var Btn_NextChapter: UIButton!
+    @IBOutlet weak var Btn_NextChapter: UIButton! {
+        didSet{
+            Btn_NextChapter.layer.cornerRadius = 23
+            Btn_NextChapter.layer.masksToBounds = true
+        }
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Intermezzo_Thumbnail.image = UIImage(named: "Intermezzo_Calender"
-        )
+        Intermezzo_Thumbnail.image = UIImage(named: "Intermezzo_Calender")
+        
     }
     
     @IBAction func Btn_NextChapter(_ sender: Any) {
